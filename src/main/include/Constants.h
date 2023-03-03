40f26bcb893907c71170a422a6c7d562ae95e97d
InspireNC-Organization <inspirenc@inspirenc.us>
2023-03-03 17:50:13 -0500

    new drivetrain ports (not tested)

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -18,13 +18,13 @@
 
 namespace drivetrain {
     // Temporary port numbers; change later.
-    const int kLeftDriveSparkPort1 = 1;
-    const int kLeftDriveSparkPort2 = 2;
-    const int kLeftDriveSparkPort3 = 3;
+    const int kLeftDriveSparkPort1 = 19;
+    const int kLeftDriveSparkPort2 = 18;
+    const int kLeftDriveSparkPort3 = 17;
 
-    const int kRightDriveSparkPort1 = 19;
-    const int kRightDriveSparkPort2 = 18;
-    const int kRightDriveSparkPort3 = 17; 
+    const int kRightDriveSparkPort1 = 1;
+    const int kRightDriveSparkPort2 = 2;
+    const int kRightDriveSparkPort3 = 3; 
 
     const int kWheelDiameter = 6;   //inches
     
