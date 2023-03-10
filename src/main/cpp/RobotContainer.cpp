64c7e88818cc4eafbbff6e361f579d8d629c6bbb
William Feng <wsfeng714@gmail.com>
2023-03-10 16:23:43 -0500

    added correct buttons

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -17,42 +17,28 @@ RobotContainer::RobotContainer() : m_autonomousCommand(&m_subsystem) {
 
 // change button bindings LATER
 void RobotContainer::ConfigureButtonBindings() {
-  // Configure your button bindings here
-  //mapping button numbers
-  frc2::JoystickButton flipDriveTrain(&m_joystick, 7);
-  frc2::JoystickButton gyroBalance(&m_joystick, 3);
-  frc2::JoystickButton elbowRight(&m_joystick, 4);
-  frc2::JoystickButton elbowLeft(&m_joystick, 6);
-  frc2::JoystickButton gyroBalance(&m_joystick, 4);
-  frc2::JoystickButton extend(&m_joystick, 10);
-  frc2::JoystickButton retract(&m_joystick, 11);
-  frc2::JoystickButton intake(&m_joystick, 1);    
-  frc2::JoystickButton outtake(&m_joystick, 2);
-  //change these button
-  frc2::JoystickButton twistRight(&m_joystick, 8);
-  frc2::JoystickButton twistLeft(&m_joystick, 9);
-
-
-  elbowRight.WhileHeld(new MoveElbow(&m_elbow, true));
-  elbowLeft.WhileHeld(new MoveElbow(&m_elbow, false));
+   //drivetrain
+  frc2::JoystickButton flipDriveTrain(&m_joystick, 8);
   flipDriveTrain.WhenPressed(new FlipDrivetrain(&m_drivetrain));
-  gyroBalance.WhileHeld(new Gyro(&m_drivetrain));
-  extend.WhileHeld(new Extend(&m_arm, true));
-  retract.WhileHeld(new Extend(&m_arm, false));  
+  
+  //gyro control
+  //frc2::JoystickButton gyroBalance(&m_joystick, 6);
+  //gyroBalance.WhileHeld(new Gyro(&m_drivetrain));
+
+  //intake/outtake
+  frc2::JoystickButton intake(&m_joystick, 1);
   intake.WhileHeld(new IntakeItem(&m_intake));
+  frc2::JoystickButton outtake(&m_joystick, 2);
   outtake.WhileHeld(new OuttakeItem(&m_intake));
-  twistLeft.WhileHeld(new Twist(&m_wrist, false));
-  twistLeft.WhileHeld(new Twist(&m_wrist, true));
-  // need to add limelight and wrist commands/subsystems
 
   //preset elevator stages
-  frc2::JoystickButton elevatorLowHeight(&m_joystick, 12);
+  frc2::JoystickButton elevatorLowHeight(&m_joystick,12);
   elevatorLowHeight.WhenPressed(new SetElevatorHeight(&m_elevator, 0, m_joystickArm.GetThrottle()));
-  frc2::JoystickButton elevatorMidHeight(&m_joystick, 10);
+  frc2::JoystickButton elevatorMidHeight(&m_joystick,10);
   elevatorMidHeight.WhenPressed(new SetElevatorHeight(&m_elevator, 1, m_joystickArm.GetThrottle()));
-  frc2::JoystickButton elevatorHighHeight(&m_joystick, 11);
+  frc2::JoystickButton elevatorHighHeight(&m_joystick,11);
   elevatorHighHeight.WhenPressed(new SetElevatorHeight(&m_elevator, 2, m_joystickArm.GetThrottle()));
-  frc2::JoystickButton elevatorPlayerHeight(&m_joystick, 9);
+  frc2::JoystickButton elevatorPlayerHeight(&m_joystick,9);
   elevatorPlayerHeight.WhenPressed(new SetElevatorHeight(&m_elevator, 3, m_joystickArm.GetThrottle()));
 
   //elevator manual
@@ -61,12 +47,23 @@ void RobotContainer::ConfigureButtonBindings() {
   frc2::JoystickButton elevatorDown(&m_joystick, 4);
   elevatorDown.WhileHeld(new ElevatorMotion(&m_elevator, false, m_joystickArm.GetThrottle()));
 
+  //arm telescope
+  frc2::JoystickButton wrenchIn(&m_joystickArm, 1);
+  wrenchIn.WhenHeld(new Extend(&m_arm, true));
+  frc2::JoystickButton wrenchOut(&m_joystickArm, 2);
+  wrenchOut.WhenHeld(new Extend(&m_arm, false));
 
+  //wrist movement
+  frc2::JoystickButton wristRight(&m_joystickArm, 5);
+  wristRight.WhenHeld(new Twist(&m_wrist, true));
+  frc2::JoystickButton wristLeft(&m_joystickArm, 3);
+  wristLeft.WhenHeld(new Twist(&m_wrist, false));
 
-
-  //maping button functions  
-  flipDriveTrain.WhenPressed(new FlipDrivetrain(&m_drivetrain));
-  gyroBalance.WhileHeld(new Gyro(&m_drivetrain));
+  //elbow movement
+  frc2::JoystickButton elbowRight(&m_joystick, 4);
+  elbowRight.WhileHeld(new MoveElbow(&m_elbow, true));
+  frc2::JoystickButton elbowLeft(&m_joystick, 6);
+  elbowLeft.WhileHeld(new MoveElbow(&m_elbow, false));
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() { 
